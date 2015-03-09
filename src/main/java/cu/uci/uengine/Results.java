ed0c598928f6af31e806d481486539beb51a317f
Leandro <lgvallejo@uci.cu>
2015-03-09 09:22:05 -0400

    Hola

--- src/main/java/cu/uci/uengine/Results.java
+++
@@ -1,8 +0,0 @@
-package cu.uci.uengine;
-
-public class Results {
-	public final static int RESULT = 0;
-	public final static int USER_TIME = 1;
-	public final static int CPU_TIME = 2;
-	public final static int MEMORY = 3;
-}
